7e59d7827099481314967f7430a1aec5b142df87
Rechra001 <tkropf12345@gmail.com>
2024-03-19 09:41:17 +0200

    Revert "Change the connection string to point to a local server database."
    This reverts commit d11e2351ecc33db6b10c98d2227c187a84f72ee7.

--- ContentManagementApp/ContentManagement.BlazorServer/Program.cs
+++ ContentManagementApp/ContentManagement.BlazorServer/Program.cs
@@ -23,13 +23,12 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("ContentManagementAccessDB") ?? throw new InvalidOperationException("Connection string 'ContentManagementAccessDB' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
