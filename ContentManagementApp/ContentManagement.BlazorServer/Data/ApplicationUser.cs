b83089d0d25844d7f7aebc37a354a03b6ed8f8c8
Rechra001 <tkropf12345@gmail.com>
2024-03-20 08:53:14 +0200

    First and last name added to access control DB

--- ContentManagementApp/ContentManagement.BlazorServer/Data/ApplicationUser.cs
+++ ContentManagementApp/ContentManagement.BlazorServer/Data/ApplicationUser.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContentManagement.BlazorServer.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+        [StringLength(100)]
+        public string? LastName { get; set; }
     }
 
 }
