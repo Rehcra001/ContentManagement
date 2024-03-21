5f0e9c9ba738622aca2af21310633cebf4ea48d4
Rechra001 <tkropf12345@gmail.com>
2024-03-21 04:18:01 +0200

    Implemented IEmailSender

--- ContentManagementApp/ContentManagement.BlazorServer/Services/EmailService.cs
+++ ContentManagementApp/ContentManagement.BlazorServer/Services/EmailService.cs
@@ -1,15 +1,16 @@
 ﻿using ContentManagement.BlazorServer.Options;
 using ContentManagement.BlazorServer.Services.Contracts;
 using ContentManagement.Models;
-using Microsoft.Extensions.Options;
 using MailKit.Net.Smtp;
 using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
 using MimeKit;
 
 
 namespace ContentManagement.BlazorServer.Services
 {
-    public class EmailService : IEmailService
+    public class EmailService : IEmailService, IEmailSender
     {
         private readonly EmailConfiguration _emailConfiguration;
 
@@ -26,11 +27,31 @@ namespace ContentManagement.BlazorServer.Services
             email.From.Add(MailboxAddress.Parse(_emailConfiguration.Username));
             email.To.Add(MailboxAddress.Parse(emailRequest.To));
             email.Subject = emailRequest.Subject;
+            builder.HtmlBody = string.Format(emailRequest.Body);
+            email.Body = builder.ToMessageBody();
 
             using var smtp = new SmtpClient();
             await smtp.ConnectAsync(_emailConfiguration.Host, _emailConfiguration.Port, SecureSocketOptions.StartTls);
             await smtp.AuthenticateAsync(_emailConfiguration.Username, _emailConfiguration.Password);
             await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var message = new MimeMessage();
+            var builder = new BodyBuilder();
+
+            message.From.Add(MailboxAddress.Parse(_emailConfiguration.Username));
+            message.To.Add(MailboxAddress.Parse(email));
+            message.Subject = subject;
+            builder.HtmlBody = string.Format(htmlMessage);
+            message.Body = builder.ToMessageBody();
+
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(_emailConfiguration.Host, _emailConfiguration.Port, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_emailConfiguration.Username, _emailConfiguration.Password);
+            await smtp.SendAsync(message);
             await smtp.DisconnectAsync(true);
         }
     }
