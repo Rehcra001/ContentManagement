537cfdc79cb88f6fdaa45693a5459e6f5a018346
Rechra001 <tkropf12345@gmail.com>
2024-03-25 16:16:47 +0200

    Added validation attribute to ensure that there are no duplicate email addresses

---
+++ ContentManagementApp/ContentManagement.BlazorServer/Data/UserValidators/User_UniqueEmailAddressAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContentManagement.BlazorServer.Data.UserValidators
+{
+    public class User_UniqueEmailAddressAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null || String.IsNullOrWhiteSpace(value.ToString()))
+            {
+                return new ValidationResult("Email address is required");
+            }
+
+            var context = (ApplicationDbContext)validationContext.GetRequiredService(typeof(ApplicationDbContext));
+
+            var user = context.Users.FirstOrDefault(x => x.Email == value.ToString());
+
+            if (user != null)
+            {
+                return new ValidationResult("Email address already exists. Please use the login in page with your password");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
