b051445eee9fd8b48734d4474c36e0fba02889ff
Rechra001 <tkropf12345@gmail.com>
2024-03-25 11:53:44 +0200

    Added a unique constraint for the email address

--- ContentManagementApp/ContentManagement.BlazorServer/Data/ApplicationDbContext.cs
+++ ContentManagementApp/ContentManagement.BlazorServer/Data/ApplicationDbContext.cs
@@ -1,9 +1,19 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContentManagement.BlazorServer.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>()
+                .HasAlternateKey(k => k.Email);
+
+        }
     }
+
+   
 }
