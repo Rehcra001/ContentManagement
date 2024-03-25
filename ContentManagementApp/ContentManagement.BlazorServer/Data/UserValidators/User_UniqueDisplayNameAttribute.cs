b163a2e58c8728800e4815cf936af82ef040567d
Rechra001 <tkropf12345@gmail.com>
2024-03-25 16:15:58 +0200

    Added Validation attribute to ensure that the display name is unique as this name will be present on any comment made by the user

---
+++ ContentManagementApp/ContentManagement.BlazorServer/Data/UserValidators/User_UniqueDisplayNameAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContentManagement.BlazorServer.Data.UserValidators
+{
+    public class User_UniqueDisplayNameAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null || String.IsNullOrWhiteSpace(value.ToString()))
+            {
+                return new ValidationResult("Display name is required");
+            }
+
+            var context = (ApplicationDbContext)validationContext.GetRequiredService(typeof(ApplicationDbContext));
+
+            var user = context.Users.FirstOrDefault(x => x.DisplayName == value.ToString());
+            
+            if (user != default)
+            {
+                return new ValidationResult("Display name already exits. Please enter a new name");
+            }
+
+            return ValidationResult.Success;
+
+        }
+    }
+}
