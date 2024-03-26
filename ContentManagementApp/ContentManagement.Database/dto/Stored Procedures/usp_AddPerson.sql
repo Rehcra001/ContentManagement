03c1f4e598d37bf4665df7846d3ccd1f9ff2171d
Rechra001 <tkropf12345@gmail.com>
2024-03-26 09:53:12 +0200

    Stored Procedure to add a new person to the People table in ContentManagementDB

---
+++ ContentManagementApp/ContentManagement.Database/dto/Stored Procedures/usp_AddPerson.sql
@@ -0,0 +1,35 @@
+﻿CREATE PROCEDURE [dbo].[usp_AddPerson]
+(
+	@UserName NVARCHAR(100),
+	@DisplayName NVARCHAR(50),
+	@Role NVARCHAR(20)
+)AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN
+			SET NOCOUNT ON;
+
+			--Add a new person
+			IF NOT EXISTS (SELECT * FROM dbo.People WHERE UserName = @UserName)
+			BEGIN
+				INSERT INTO dbo.People (UserName, DisplayName, Role)
+				VALUES (@UserName, @DisplayName, @Role);
+			END
+
+			ELSE
+			BEGIN
+				DECLARE @MessageText NVARCHAR(50);
+				SET @MessageText = FORMATMESSAGE(51000, N'Record already exists');
+				THROW 51000, @MessageText, 1;
+			END;
+		COMMIT TRAN;
+	END TRY
+
+	BEGIN CATCH
+		IF (@@TRANCOUNT > 0)
+		BEGIN
+			ROLLBACK TRAN;
+		END;
+	END CATCH;
+END;
+GO
