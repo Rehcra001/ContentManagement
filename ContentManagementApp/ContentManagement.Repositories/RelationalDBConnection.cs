c87c2c89c824a89beb931bff8ea1b72a68d0c594
Rechra001 <tkropf12345@gmail.com>
2024-03-27 08:44:15 +0200

    Sql connection to ContentManagementDB

---
+++ ContentManagementApp/ContentManagement.Repositories/RelationalDBConnection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ContentManagement.Repositories
+{
+    public class RelationalDBConnection : IRelationalDBConnection
+    {
+        private readonly IConfiguration _config;
+
+        public RelationalDBConnection(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection sqlConnection()
+        {
+            return new SqlConnection(_config.GetConnectionString("ContentManagementDB"));
+        }
+    }
+}
