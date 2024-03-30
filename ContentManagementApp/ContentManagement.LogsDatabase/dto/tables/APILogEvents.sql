aa8211809f0e06c019691e53792f70f18fd10bf9
Rechra001 <tkropf12345@gmail.com>
2024-03-30 13:12:39 +0200

    Table added to log any exception etc when accessing the ContentManagement.API

---
+++ ContentManagementApp/ContentManagement.LogsDatabase/dto/tables/APILogEvents.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[APILogEvents]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Message] NVARCHAR(MAX) NULl,
+	[MessageTemplate] NVARCHAR(MAX) NULL,
+	[Level] NVARCHAR(128) NULL,
+	[TimeStamp] DATETIME2 NOT NULL,
+	[Exception] NVARCHAR(MAX) NULL,
+	[Properties] NVARCHAR(MAX) NULL
+)
