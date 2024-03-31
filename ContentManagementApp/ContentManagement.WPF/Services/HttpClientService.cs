4caa36fda55985ce8219efa91414c3a14723da60
Rechra001 <tkropf12345@gmail.com>
2024-03-31 14:43:52 +0200

    Http Client service implemented

---
+++ ContentManagementApp/ContentManagement.WPF/Services/HttpClientService.cs
@@ -0,0 +1,30 @@
+﻿using ContentManagement.WPF.Services.Contracts;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ContentManagement.WPF.Services
+{
+    public class HttpClientService : IHttpClientService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpClient HttpClient { get => _httpClient; }
+
+        private readonly IConfiguration _config;
+
+        public HttpClientService(IConfiguration config)
+        {
+            _config = config;
+            _httpClient = new HttpClient();
+            InitialiseClient();
+        }
+
+        private void InitialiseClient()
+        {
+            HttpClient.BaseAddress = new Uri(_config.GetSection("ApiBaseAddress").Value!);
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
