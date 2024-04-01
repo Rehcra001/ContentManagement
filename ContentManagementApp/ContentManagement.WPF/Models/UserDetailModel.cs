8e293d3bdc129a96396baa9a268b1cbb8b08b8f7
Rechra001 <tkropf12345@gmail.com>
2024-04-01 09:58:49 +0200

    Holds the users details retrieved upon logging in

---
+++ ContentManagementApp/ContentManagement.WPF/Models/UserDetailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContentManagement.WPF.Models
+{
+    public class UserDetailModel
+    {
+        public string? UserName { get; set; } = "";
+        public string? EmailAddress { get; set; } = "";
+        public string? FirstName { get; set; } = "";
+        public string? LastName { get; set; } = "";
+        public string? DisplayName { get; set; } = "";
+    }
+}
