f579f0f1c4bd1290676f7ae8514dfa9dc6498bdc
Rechra001 <tkropf12345@gmail.com>
2024-04-01 09:57:37 +0200

    Service to store the users details retrieve from the JWT Token once logged in

---
+++ ContentManagementApp/ContentManagement.WPF/Services/Contracts/IUserDetailService.cs
@@ -0,0 +1,14 @@
+﻿using ContentManagement.WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement.WPF.Services.Contracts
+{
+    public interface IUserDetailService
+    {
+        UserDetailModel UserDetailModel { get; set; }
+    }
+}
---
+++ ContentManagementApp/ContentManagement.WPF/Services/UserDetailService.cs
@@ -0,0 +1,11 @@
+﻿using ContentManagement.WPF.Models;
+using ContentManagement.WPF.Services.Contracts;
+
+namespace ContentManagement.WPF.Services
+{
+    public class UserDetailService : IUserDetailService
+    {
+        private UserDetailModel _userDetailModel = new UserDetailModel();
+        public UserDetailModel UserDetailModel { get => _userDetailModel; set => _userDetailModel = value; }
+    }
+}
