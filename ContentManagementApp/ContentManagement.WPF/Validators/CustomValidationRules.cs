a6a03f5acfb9897d22758be4d3d4d7fbaff2be92
Rechra001 <tkropf12345@gmail.com>
2024-04-03 08:53:45 +0200

    Fluent Validation added to the project

---
+++ ContentManagementApp/ContentManagement.WPF/Validators/CustomValidationRules.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement.WPF.Validators
+{
+    internal class CustomValidationRules
+    {
+        internal bool BeAValidEmailCharacter(string email)
+        {
+            if (email.Contains(';'))
+            {
+                return false;
+            }
+            if (email.Contains("--"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        internal bool BeAValidName(string name)
+        {
+            name = name.Replace(" ", "");
+            name = name.Replace("-", "");
+
+            return name.All(char.IsLetter);
+        }
+    }
+}
---
+++ ContentManagementApp/ContentManagement.WPF/Validators/UserSignInValidator.cs
@@ -0,0 +1,33 @@
+﻿using ContentManagement.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement.WPF.Validators
+{
+    public class UserSignInValidator : AbstractValidator<UserSignInDTO>
+    {
+        CustomValidationRules _vr = new CustomValidationRules();
+
+        public UserSignInValidator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            //Email Address
+            RuleFor(u => u.EmailAddress)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty.")
+                .MaximumLength(100).WithMessage("{PropertyName} may not contain more than {MaxLength} characters.")
+                .EmailAddress().WithMessage("{PropertyName} is not valid.")
+                .Must(_vr.BeAValidEmailCharacter).WithMessage("{PropertyName} contains invalid characters! ';' or '--'");
+
+            //Password
+            RuleFor(u => u.Password)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty.")
+                .MinimumLength(8).WithMessage("{PropertyName} must contain at least {MinLength} characters.")
+                .MaximumLength(80).WithMessage("{PropertyName} may not contain more than {MaxLength} characters.");
+        }
+    }
+}
