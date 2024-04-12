67fdc375272223f46403795122c80a315ccf64ce
Rechra001 <tkropf12345@gmail.com>
2024-04-12 12:46:29 +0200

    Category DTO added

---
+++ ContentManagementApp/ContentManagement.DTOs/CategoryDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public bool IsPublished { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModified { get; set; }
+        public DateTime PublishedOn { get; set; }
+    }
+}
