fe0c636097f209348fe4a7cf29527542de4c87f1
Rechra001 <tkropf12345@gmail.com>
2024-04-17 08:37:40 +0200

    Sub category DTO added

---
+++ ContentManagementApp/ContentManagement.DTOs/SubCategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement.DTOs
+{
+    public class SubCategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public bool IsPublished { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModified { get; set; }
+        public DateTime PublishedOn { get; set; }
+    }
+}
