9df3b2924e7153434899b15e9390c5366b8f103e
Rechra001 <tkropf12345@gmail.com>
2024-04-17 09:26:53 +0200

    Sub Category validator added

---
+++ ContentManagementApp/ContentManagement.WPF/Validators/SubCategoryValidator.cs
@@ -0,0 +1,32 @@
+﻿using ContentManagement.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentManagement.WPF.Validators
+{
+    public class SubCategoryValidator : AbstractValidator<SubCategoryDTO>
+    {
+        CustomValidationRules _vr = new CustomValidationRules();
+        public SubCategoryValidator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty")
+                .MaximumLength(50).WithMessage("{PropertyName} cannot be more than {MaxLength} characters");
+
+            RuleFor(x => x.Description)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty")
+                .MaximumLength(50).WithMessage("{PropertyName} cannot be more than {MaxLength} characters");
+
+            RuleFor(x => x.CreatedOn)
+                .Must(_vr.BeAValidDate).WithMessage("{PropertyName} is not valid");
+
+
+        }
+    }
+}
